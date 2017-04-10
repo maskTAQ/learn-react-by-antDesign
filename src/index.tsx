7f2da7b495f79fce096bad350dab68cad50e542e
taq <50889574@qq.com>
2017-04-10 14:02:03 +0800

    上传style文件夹

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,28 @@
-import  React from "react";
-import  ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 
+import Button from "./components/button";
 
-import Button  from "./components/button";
+import './demostyle/index.less';
 
-ReactDOM.render(
-	<Button 
-	type="danger" 
-	data-ss="ss">ss</Button>,
-	document.getElementById("example")
-	);
+const createButton = () => {
+    return (
+        <div>
+            <Button type="danger" data-text="自定义属性">danger</Button>
+            <Button type="ghost" size="small">ghost</Button>
+        </div>
+    )
+}
+
+const createContainer = () => {
+    return (
+        <div className="container">
+            <h2>button</h2>
+            <div className="content">
+                {createButton()}
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(createContainer(), document.getElementById("example"));
